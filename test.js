829798f73cf8fb30fc296ecea10069993e4bb71e
Nicholas Rubright <koalrube@gmail.com>
2021-07-09 22:49:35 -0400

    added more functions to SearchEndpoint

--- test.js
+++ test.js
@@ -2,6 +2,11 @@ const Botify = require('./lib/Botify');
 
 let botify = new Botify('API_KEY');
 
-botify.Search.gettingByKeyword("tania bowra", "artist")
-    .then(res => console.log(res.data))
-    .catch(err => console.error(err));
+// botify.Search.gettingByKeyword("tania bowra", "artist")
+//     .then(res => console.log(res.data))
+//     .catch(err => console.error(err));
+
+
+botify.Search.gettingArtistByKeyword("bbno$")
+    .then(response => console.log(response))
+    .catch(err => console.error("error: " + err));
