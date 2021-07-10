d81cd5834301bb50d3e39f7bea43514c21a44dd4
Nicholas Rubright <koalrube@gmail.com>
2021-07-09 22:29:31 -0400

    successfully sent my first api request and received payload

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
 const Botify = require('./lib/Botify');
 
-let myTest = Botify();
+let botify = new Botify('API_KEY');
+
+botify.Search.gettingByKeyword("tania bowra", "artist")
+    .then(res => console.log(res.data))
+    .catch(err => console.error(err));
