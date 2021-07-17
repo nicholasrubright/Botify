be51b042469789327249a5ab4cab202afd5e43d5
Nicholas Rubright <koalrube@gmail.com>
2021-07-16 20:06:12 -0400

    renamed the main class and updated query url builder

--- test.js
+++ test.js
@@ -1,19 +1,21 @@
-const Botify = require('./lib/Botify');
+const BotifyJS = require('./lib/BotifyJS');
 
-let botify = new Botify('API_KEY');
+let Botify = new BotifyJS('API_KEY');
 
+let test = Botify.Artists.gettingArtistTopTracksById('dkdkd');
 
-
-botify.Search.gettingTrackByKeyword("Best Thing")
-    .then(track_info => {
-        const id = track_info.tracks.items[0].id
+// botify.Search.gettingTrackByKeyword("Best Thing")
+//     .then(track_info => {
+//         const id = track_info.tracks.items[0].id
         
-        botify.Tracks.gettingTrackById(id)
-            .then(data => {
-                console.log(data);
-            })
+//         botify.Tracks.gettingTrackById(id)
+//             .then(data => {
+//                 console.log(data);
+//             })
 
-    });
+//     });
+
+
 
 //let test = botify.Search.gettingTrackByKeyword("Best Thing");
 
